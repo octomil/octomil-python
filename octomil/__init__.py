477edd4a06a3fb1476bd7d3ef7706adb36931074
Sean B <bsudarshan@outlook.com>
2026-03-25 12:57:22 -0400

    chore: prepare release 4.4.0 (#344)

--- octomil/__init__.py
+++ octomil/__init__.py
@@ -9,7 +9,7 @@ Submodules are aliased here so ``from octomil.secagg import …`` works.
 
 from __future__ import annotations
 
-__version__ = "4.3.0"
+__version__ = "4.4.0"
 
 import importlib as _importlib
 import logging as _logging
