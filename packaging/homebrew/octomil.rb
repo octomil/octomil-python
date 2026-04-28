ab06cc31ff4f1629ae055690af732955af503726
sbangalore <bsudarshan@outlook.com>
2026-04-27 22:36:20 -0700

    chore: prepare release 4.12.2

--- packaging/homebrew/octomil.rb
+++ packaging/homebrew/octomil.rb
@@ -16,7 +16,7 @@
 class Octomil < Formula
   desc "Serve, deploy, and observe ML models on edge devices"
   homepage "https://octomil.com"
-  version "4.12.1"
+  version "4.12.2"
   license "MIT"
 
   on_macos do
